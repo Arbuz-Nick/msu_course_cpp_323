1afb2608d3c0c282364104f5f2d057527fce86cd
Arbuz-Nick <anr.qwert@gmail.com>
2021-12-27 12:47:14 +0300

    ch tsk9

---
+++ nikolay_arbuzov/game_printing.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include "game.hpp"
+
+namespace printing {
+
+std::string print_game(const Game& game);
+
+}  // namespace printing
---
+++ nikolay_arbuzov/game_printing.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include "game.hpp"
+
+namespace printing {
+
+std::string print_game(const Game& game);
+
+}  // namespace printing
